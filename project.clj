42aba00f0f4ceae8b8c9ccebae356713dd638f0b
OHTA Shogo <athos0220@gmail.com>
2017-07-04 18:34:02 +0900

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject specium "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Inverse of clojure.spec(.alpha)/form, reducing eval calls as much as possible"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
+                 [org.clojure/clojurescript "1.9.671"]])
