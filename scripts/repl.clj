0c2c9af446848e03e3df599b6548bdcc89a2ec34
OHTA Shogo <athos0220@gmail.com>
2017-08-04 06:50:19 +0900

    Add script to start up CLJS REPL

---
+++ scripts/repl.clj
@@ -0,0 +1,8 @@
+(require '[cljs.repl :as repl]
+         '[cljs.repl.node :as node])
+
+(repl/repl* (node/repl-env)
+  {:output-dir "target/out"
+   :optimizations :none
+   :cache-analysis true
+   :source-map true})
