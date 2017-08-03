b584f607bdd09c45c1b28710db71f850107ed155
OHTA Shogo <athos0220@gmail.com>
2017-08-04 05:44:30 +0900

    Update URL in project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject specium "0.1.0-SNAPSHOT"
   :description "Inverse of s/form, reducing eval calls as much as possible"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/athos/specium"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
