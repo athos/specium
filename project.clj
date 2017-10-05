ea3bee50b1cf33791f8abfeead6a04906c5d581b
OHTA Shogo <athos0220@gmail.com>
2017-10-05 18:57:22 +0900

    Bump CLJ & CLJS

--- project.clj
+++ project.clj
@@ -4,8 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.9.0-alpha19" :scope "provided"]
-                 [org.clojure/clojurescript "1.9.908" :scope "provided"]]
+  :dependencies [[org.clojure/clojure "1.9.0-beta1" :scope "provided"]
+                 [org.clojure/clojurescript "1.9.946" :scope "provided"]]
 
   :test-paths ["test/cljc"]
 
